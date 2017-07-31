d69583c6319bb05c85625e2d785127192a02168d
Abarajithan <abar.nerve24@gmail.com>
2017-07-31 17:43:40 +0530

    Undefined capacity handling

--- hub_/calendar/js/fullcalendar.js
+++ hub_/calendar/js/fullcalendar.js
@@ -3251,7 +3251,7 @@ function ResourceView(element, calendar, viewName) {
         for (i=0; i<colCnt; i++) {
             date = resourceDate(i);
             headCell = dayHeadCells.eq(i);
-            headCell.html(resources[i].name+" ("+resources[i].capacity+")");
+            headCell.html(resources[i].capacity != undefined ? resources[i].name+" ("+resources[i].capacity+")" : resources[i].name);
             headCell.attr("id", resources[i].id);
             bodyCell = dayBodyCells.eq(i);
             if (+date == +today) {
